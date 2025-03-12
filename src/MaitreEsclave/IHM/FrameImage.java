11c281d9ead6bceffc86947dc69ba5fab2ee5781
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2025-03-12 16:21:50 +0100

    maj

--- src/MaitreEsclave/IHM/FrameImage.java
+++ src/MaitreEsclave/IHM/FrameImage.java
@@ -17,7 +17,7 @@ public class FrameImage extends JFrame
 		this.ctrl = ctrl;
 
 		this.setTitle("Image");
-		this.setSize(750, 400);
+		this.setSize(800, 400);
 		this.setLocationRelativeTo(this.panelImage);
 		this.setResizable(false);
 
