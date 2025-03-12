8dbdea732a1abfba46bea9400a51dbdce7773cb1
Julien Oyer <oyer.ludovic@gmail.com>
2025-03-12 14:57:03 +0100

    Ajout de la gestion des images dans la classe Serveur avec initialisation des grilles d'images et création du socket serveur

--- src/MaitreEsclave/Metier/Serveur.java
+++ src/MaitreEsclave/Metier/Serveur.java
@@ -1,15 +1,49 @@
 package MaitreEsclave.Metier;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
 public class Serveur
 {
-	private Boolean[][] grilleImagesComplété;
-	private ImageIcon[][]       grilleImages;
+	private Boolean[][] grilleImagesComplete;
+	private BufferedImage[][]   grilleImages;
+	private DatagramSocket      serverSocket;
 
-	public Serveur(String cheminImage,int nbColonnes, int nbLignes)
+	public Serveur(String cheminImage,int nbColonnes, int nbLignes, int port)
 	{
-		this.grilleImagesComplété = new Boolean[nbColonnes][nbLignes];
-		this.grilleImages = new ImageIcon[nbColonnes][nbLignes];
+		BufferedImage image = null;
+		try
+		{
+			this.serverSocket = new DatagramSocket(port,InetAddress.getByName());
+
+			image = ImageIO.read(new File(cheminImage));
+			if (image == null)
+			{
+				throw new IOException("Impossible de lire l'image");
+			}
+		}
+		catch (IOException e){e.printStackTrace();}
+
+		this.grilleImagesComplete = new Boolean[nbLignes][nbColonnes];
+		this.grilleImages   = new BufferedImage[nbLignes][nbColonnes];
+
+		ImageIcon imageIcon = new ImageIcon(image);
+
+		int largeurImage = imageIcon.getIconWidth() / nbColonnes;
+		int hauteurImage = imageIcon.getIconHeight() / nbLignes;
+
+		for (int i = 0; i < nbColonnes; i++)
+		{
+			for (int j = 0; j < nbLignes; j++)
+			{
+				this.grilleImages[i][j] = image.getSubimage(i * largeurImage, j * hauteurImage, largeurImage, hauteurImage);
+				this.grilleImagesComplete[i][j] = false;
+			}
+		}
 	}
 }
