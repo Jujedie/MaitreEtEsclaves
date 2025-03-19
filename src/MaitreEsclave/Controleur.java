1d3f1afe71115bd1661a26f4760f8843d71ea61c
Nahel <nahel.kochat@laposte.net>
2025-03-19 14:13:23 +0100

    Correction controleur et compile.list

--- src/MaitreEsclave/Controleur.java
+++ src/MaitreEsclave/Controleur.java
@@ -23,43 +23,15 @@ public class Controleur
 
 		this.frameImage = new FrameImage(this, serveur.getGrilleImages());
 
-		try
-		{
-			// Lit le fichier et le transforme en image
-			FileInputStream fichier = new FileInputStream("./" + nomFichier);
-			BufferedImage image = ImageIO.read(fichier);
+		
 
-			// Initialisation du tableau des images
-			BufferedImage imgs[][] = new BufferedImage[nbLig][nbCol];
-
-			// Division de l'image en sous-images égales
-			int largeurSousImage = image.getWidth() / nbCol;
-			int hauteurSousImage = image.getHeight() / nbLig;
-
-			// Itération des lignes et colonnes pour chaque sous-image
-			for (int cpt1 = 0; cpt1 < nbLig; cpt1++)
-			{
-				for (int cpt2 = 0; cpt2 < nbCol; cpt2++)
-				{
-					// Création sous-image
-					imgs[cpt1][cpt2] = new BufferedImage(largeurSousImage, hauteurSousImage, image.getType());
-				}
-			}
-
-			//TODO: Utilisation des sous-images
-		}
-
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
 	}
 
 	public static void main(String[] args) 
 	{
 		if (args.length != 4)
 		{
-			System.out.println("Erreur de format : java Controleur nomImage nombreColonnes nombreLignes port");
+			System.out.println("Erreur de format : java Controleur nomImage nombreLignes nombreColonnes port");
 		}
 
 		else
@@ -76,4 +48,6 @@ public class Controleur
 			}
 		}
 	}
+
+	
 }
